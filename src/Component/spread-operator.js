ecef0220fcb65d645ac5893dca9200da997c375d
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-03-15 13:17:52 -0500

    functional component

---
+++ src/Component/spread-operator.js
@@ -0,0 +1,13 @@
+function SpreadOperator() {
+  return (
+    <div>
+      <h2>ES6 Spread Operator</h2>
+      <p>
+        The JavaScript spread operator (...) allows us to quickly copy all or
+        part of an existing array or object into another array or object.
+      </p>
+    </div>
+  );
+}
+
+export default SpreadOperator;
